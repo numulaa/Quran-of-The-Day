20f1b913c8c5c84a47b88a70f5262b66a936e39f
Nurul Mukhlisa <numulaa@gmail.com>
2022-06-01 10:43:52 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+let btn = document.querySelector(".btn").addEventListener("click", getAdvice);
+function getAdvice() {
+  //   let surah = Math.ceil(Math.random() * 114);
+  let ayah = Math.ceil(Math.random() * 6232);
+  fetch(`http://api.alquran.cloud/v1/ayah/${ayah}/quran-uthmani `)
+    .then((res) => res.json())
+    .then((result) => {
+      document.querySelector(".advice-content").innerText = result.data.text;
+      document.querySelector("#advice-id").innerText =
+        result.data.numberInSurah;
+      document.querySelector("#surah-name").innerText = result.data.surah.name;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+  fetch(`http://api.alquran.cloud/v1/ayah/${ayah}/id.indonesian`)
+    .then((res) => res.json())
+    .then((result) => {
+      document.querySelector("#ayah-translation").innerText = result.data.text;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
+
+document.querySelector(".light-icon").addEventListener("click", lightMode);
+function lightMode() {
+  document.querySelector("body").classList.toggle("light-mode");
+  document.querySelector(".light-icon").classList.toggle("light-mode");
+  document.querySelector(".feedback p").classList.toggle("light-mode-text");
+}
